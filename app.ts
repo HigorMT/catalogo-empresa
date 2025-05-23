afbbd92cf8956803d24b627f0f1220220d57b170
HigorMT <51681422+HigorMT@users.noreply.github.com>
2025-05-22 23:57:58 -0400

    Ajuste layout e integração com formulário de prod

--- app.ts
+++ app.ts
@@ -1,8 +1,23 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxtJC9vK4lAMUwjVugESkPGHjCQhQS0NhKLrkon6dU_lMZS5OQPa0eT9NlGk94NHTAcYg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby4uj7txE2mCzngQPCOJB7AE87jDvooiOFIXE8BUOowndZ8apZb4mOGHzGooG-iYhZg/exec";
 
 class DadosPlanilha {
     header?: string;
     value?: string;
+}
+
+class BasicData {
+    endereco_da_empresa?: DadosPlanilha;
+    nome_da_empresa?: DadosPlanilha;
+    telefone?: DadosPlanilha;
+    e_mail?: DadosPlanilha;
+}
+
+class CompleteData extends BasicData {
+    tempo_de_atuacao_e_de_experiencia?: DadosPlanilha;
+    descricao_dos_servicos_prestados?: DadosPlanilha;
+    natureza_do_trabalho_?: DadosPlanilha;
+    area_de_atuacao_?: DadosPlanilha;
+    nome?: DadosPlanilha;
 }
 
 let dadosPlanilha: Map<string, DadosPlanilha>[] = [];
@@ -27,26 +42,71 @@ function renderizarDados(lista: Map<string, DadosPlanilha>[]): void {
     const container: HTMLElement = document.getElementById("empresa-lista")!;
     container.innerHTML = "";
 
-    lista.forEach((item: Map<string, DadosPlanilha>) => {
+    const dadosBasicos: BasicData[] = lista as BasicData[]
+
+    dadosBasicos.forEach((item: BasicData, index: number): void => {
         const card: HTMLDivElement = document.createElement("div");
         card.className = "card";
 
-        card.innerHTML = Object.entries(item)
-            .filter(([campo, { header }]: [string, any]): boolean => campo !== "carimbo_de_data_hora" && campo !== "logo")
-            .map(([campo, { header, value }]: [string, any]): string => {
-                if (campo === "logo") {
-                    const imgSrc = value?.includes("drive.google.com/open?id=")
-                        ? value.replace("open?id=", "uc?export=view&id=")
-                        : value || "/imgs/no_image.png";
+        card.innerHTML = `
+          <p id="nome_da_empresa_${index + 1}" class="nome_empresa"><strong>${item?.nome_da_empresa?.value}</strong></p>
+          <p id="telefone_da_empresa_${index + 1}"><strong>Telefone</strong> ${item?.telefone?.value}</p>
+          <p id="e_mail_da_empresa_${index + 1}"><strong>E-mail</strong> ${item?.e_mail?.value}</p>
+          <p id="endereco_da_empresa_${index + 1}"><strong>Endereço</strong> ${item?.endereco_da_empresa?.value}</p>
+        `
 
-                    return `<div class="logo"><img src="${imgSrc}" alt="logo"></div>`;
-                }
-                return `<p><strong>${header}:</strong> ${value}</p>`;
-            })
-            .join("");
+        card.addEventListener("click", (): void => abrirModal(item as any));
 
         container.appendChild(card);
     });
+}
+
+function abrirModal(item: CompleteData): void {
+    const modalOverlay: HTMLElement = document.getElementById("modal-overlay")!;
+    const modalContent: HTMLElement = document.getElementById("modal-content")!;
+
+    const whatsappNumber: string | undefined = `${item?.telefone?.value}`?.replace(/\D/g, "");
+    const link = `https://wa.me/${whatsappNumber}`;
+
+    modalContent.innerHTML = `
+    
+          <p id="nome_da_empresa" class="nome_empresa"><strong>${item?.nome_da_empresa?.value}</strong></p>
+          <p id="representante"><strong>Representante</strong> ${item?.nome?.value}</p>
+          <p id="telefone_da_empresa"><strong>Telefone</strong> ${item?.telefone?.value}</p>
+          <p id="e_mail_da_empresa"><strong>E-mail</strong> ${item?.e_mail?.value}</p>
+           <p id="area_de_atuacao_"><strong>Area de Atuação</strong> ${item?.area_de_atuacao_?.value}</p>
+          <p id="natureza_do_trabalho"><strong>Natureza da Empresa</strong> ${item?.natureza_do_trabalho_?.value}</p>
+          <p id="tempo_de_atuacao_e_de_experiencia"><strong>Tempo de Atuação</strong> ${item?.tempo_de_atuacao_e_de_experiencia?.value}</p>
+          <p id="descricao_dos_servicos_prestados"><strong>Endereço</strong> ${item?.descricao_dos_servicos_prestados?.value}</p>
+        
+          <p id="endereco_da_empresa"><strong>Endereço</strong> ${item?.endereco_da_empresa?.value}</p>
+    
+            <div class="logo-whatsapp" title="Clique para iniciar uma conversa com este contato">
+                <a href="${link}" target="_blank">
+                    <img src="./imgs/WhatsApp-logo.png" alt="logo WhatsApp" width="50" height="50">
+                </a>
+            </div>
+    `
+
+
+        const t = Object.entries(item)
+        .map(([_, {header, value}]): string => {
+            if (header?.toLowerCase().includes("telefone")) {
+                const whatsappNumber: string | undefined = `${value}`?.replace(/\D/g, "");
+                const link = `https://wa.me/${whatsappNumber}`;
+
+                return `<div class="logo-whatsapp" title="Clique para iniciar uma conversa com este contato">
+                        <a href="${link}" target="_blank">
+                            <img src="./imgs/WhatsApp-logo.png" alt="logo WhatsApp" width="50" height="50">
+                        </a>
+                    </div>`;
+            }
+
+            return `<p><strong>${header}:</strong> ${value}</p>`;
+        })
+        .join("");
+
+    modalOverlay.classList.remove("hidden");
 }
 
 document.addEventListener("DOMContentLoaded", (): void => {
@@ -59,7 +119,7 @@ document.addEventListener("DOMContentLoaded", (): void => {
         const termo: string = inputBusca.value.toLowerCase();
 
         const filtrados: Map<string, DadosPlanilha>[] = dadosPlanilha.filter((item: any): boolean => {
-            const nome: string = item["nome_empresa"]?.value?.toLowerCase() || "";
+            const nome: string = item["nome_da_empresa"]?.value?.toLowerCase() || "";
             return nome.includes(termo);
         });
 
@@ -71,3 +131,36 @@ document.addEventListener("DOMContentLoaded", (): void => {
     btnBusca.addEventListener("click", filtrar);
 
 });
+
+document.addEventListener("DOMContentLoaded", (): void => {
+    const closeModal: () => void = (): void => {
+        const modalOverlay: HTMLElement = document.getElementById("modal-overlay")!;
+        modalOverlay.classList.add("hidden");
+    };
+
+    document.getElementById("modal-close")?.addEventListener("click", closeModal);
+    document.getElementById("modal-overlay")?.addEventListener("click", (e) => {
+        if (e.target === e.currentTarget) closeModal();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", (): void => {
+    const navbar = document.getElementById('navbar') as HTMLElement;
+    const searchIcon = document.getElementById('search-icon') as HTMLElement;
+    const timesIcon = document.getElementById('times-icon') as HTMLElement;
+    const searchContainer = document.getElementById('search-container') as HTMLElement;
+
+    searchIcon.addEventListener('click', (): void => {
+        searchContainer.classList.toggle('hidden');
+        timesIcon.classList.toggle('hidden');
+        navbar.classList.toggle('expanded');
+    });
+
+    timesIcon.addEventListener('click', (): void => {
+        searchContainer.classList.toggle('hidden');
+        timesIcon.classList.toggle('hidden');
+        navbar.classList.toggle('expanded');
+    });
+
+});
+
--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,10 @@
 "use strict";
-const API_URL = "https://script.google.com/macros/s/AKfycbxtJC9vK4lAMUwjVugESkPGHjCQhQS0NhKLrkon6dU_lMZS5OQPa0eT9NlGk94NHTAcYg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby4uj7txE2mCzngQPCOJB7AE87jDvooiOFIXE8BUOowndZ8apZb4mOGHzGooG-iYhZg/exec";
 class DadosPlanilha {
+}
+class BasicData {
+}
+class CompleteData extends BasicData {
 }
 let dadosPlanilha = [];
 async function carregarDados() {
@@ -21,23 +25,62 @@ async function carregarDados() {
 function renderizarDados(lista) {
     const container = document.getElementById("empresa-lista");
     container.innerHTML = "";
-    lista.forEach((item) => {
+    const dadosBasicos = lista;
+    dadosBasicos.forEach((item, index) => {
+        var _a, _b, _c, _d;
         const card = document.createElement("div");
         card.className = "card";
-        card.innerHTML = Object.entries(item)
-            .filter(([campo, { header }]) => campo !== "carimbo_de_data_hora" && campo !== "logo")
-            .map(([campo, { header, value }]) => {
-            if (campo === "logo") {
-                const imgSrc = (value === null || value === void 0 ? void 0 : value.includes("drive.google.com/open?id="))
-                    ? value.replace("open?id=", "uc?export=view&id=")
-                    : value || "/imgs/no_image.png";
-                return `<div class="logo"><img src="${imgSrc}" alt="logo"></div>`;
-            }
-            return `<p><strong>${header}:</strong> ${value}</p>`;
-        })
-            .join("");
+        card.innerHTML = `
+          <p id="nome_da_empresa_${index + 1}" class="nome_empresa"><strong>${(_a = item === null || item === void 0 ? void 0 : item.nome_da_empresa) === null || _a === void 0 ? void 0 : _a.value}</strong></p>
+          <p id="telefone_da_empresa_${index + 1}"><strong>Telefone</strong> ${(_b = item === null || item === void 0 ? void 0 : item.telefone) === null || _b === void 0 ? void 0 : _b.value}</p>
+          <p id="e_mail_da_empresa_${index + 1}"><strong>E-mail</strong> ${(_c = item === null || item === void 0 ? void 0 : item.e_mail) === null || _c === void 0 ? void 0 : _c.value}</p>
+          <p id="endereco_da_empresa_${index + 1}"><strong>Endereço</strong> ${(_d = item === null || item === void 0 ? void 0 : item.endereco_da_empresa) === null || _d === void 0 ? void 0 : _d.value}</p>
+        `;
+        card.addEventListener("click", () => abrirModal(item));
         container.appendChild(card);
     });
+}
+function abrirModal(item) {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l;
+    const modalOverlay = document.getElementById("modal-overlay");
+    const modalContent = document.getElementById("modal-content");
+    const whatsappNumber = (_b = `${(_a = item === null || item === void 0 ? void 0 : item.telefone) === null || _a === void 0 ? void 0 : _a.value}`) === null || _b === void 0 ? void 0 : _b.replace(/\D/g, "");
+    const link = `https://wa.me/${whatsappNumber}`;
+    modalContent.innerHTML = `
+    
+          <p id="nome_da_empresa" class="nome_empresa"><strong>${(_c = item === null || item === void 0 ? void 0 : item.nome_da_empresa) === null || _c === void 0 ? void 0 : _c.value}</strong></p>
+          <p id="representante"><strong>Representante</strong> ${(_d = item === null || item === void 0 ? void 0 : item.nome) === null || _d === void 0 ? void 0 : _d.value}</p>
+          <p id="telefone_da_empresa"><strong>Telefone</strong> ${(_e = item === null || item === void 0 ? void 0 : item.telefone) === null || _e === void 0 ? void 0 : _e.value}</p>
+          <p id="e_mail_da_empresa"><strong>E-mail</strong> ${(_f = item === null || item === void 0 ? void 0 : item.e_mail) === null || _f === void 0 ? void 0 : _f.value}</p>
+           <p id="area_de_atuacao_"><strong>Area de Atuação</strong> ${(_g = item === null || item === void 0 ? void 0 : item.area_de_atuacao_) === null || _g === void 0 ? void 0 : _g.value}</p>
+          <p id="natureza_do_trabalho"><strong>Natureza da Empresa</strong> ${(_h = item === null || item === void 0 ? void 0 : item.natureza_do_trabalho_) === null || _h === void 0 ? void 0 : _h.value}</p>
+          <p id="tempo_de_atuacao_e_de_experiencia"><strong>Tempo de Atuação</strong> ${(_j = item === null || item === void 0 ? void 0 : item.tempo_de_atuacao_e_de_experiencia) === null || _j === void 0 ? void 0 : _j.value}</p>
+          <p id="descricao_dos_servicos_prestados"><strong>Endereço</strong> ${(_k = item === null || item === void 0 ? void 0 : item.descricao_dos_servicos_prestados) === null || _k === void 0 ? void 0 : _k.value}</p>
+        
+          <p id="endereco_da_empresa"><strong>Endereço</strong> ${(_l = item === null || item === void 0 ? void 0 : item.endereco_da_empresa) === null || _l === void 0 ? void 0 : _l.value}</p>
+    
+            <div class="logo-whatsapp" title="Clique para iniciar uma conversa com este contato">
+                <a href="${link}" target="_blank">
+                    <img src="./imgs/WhatsApp-logo.png" alt="logo WhatsApp" width="50" height="50">
+                </a>
+            </div>
+    `;
+    const t = Object.entries(item)
+        .map(([_, { header, value }]) => {
+        var _a;
+        if (header === null || header === void 0 ? void 0 : header.toLowerCase().includes("telefone")) {
+            const whatsappNumber = (_a = `${value}`) === null || _a === void 0 ? void 0 : _a.replace(/\D/g, "");
+            const link = `https://wa.me/${whatsappNumber}`;
+            return `<div class="logo-whatsapp" title="Clique para iniciar uma conversa com este contato">
+                        <a href="${link}" target="_blank">
+                            <img src="./imgs/WhatsApp-logo.png" alt="logo WhatsApp" width="50" height="50">
+                        </a>
+                    </div>`;
+        }
+        return `<p><strong>${header}:</strong> ${value}</p>`;
+    })
+        .join("");
+    modalOverlay.classList.remove("hidden");
 }
 document.addEventListener("DOMContentLoaded", () => {
     carregarDados();
@@ -47,11 +90,39 @@ document.addEventListener("DOMContentLoaded", () => {
         const termo = inputBusca.value.toLowerCase();
         const filtrados = dadosPlanilha.filter((item) => {
             var _a, _b;
-            const nome = ((_b = (_a = item["nome_empresa"]) === null || _a === void 0 ? void 0 : _a.value) === null || _b === void 0 ? void 0 : _b.toLowerCase()) || "";
+            const nome = ((_b = (_a = item["nome_da_empresa"]) === null || _a === void 0 ? void 0 : _a.value) === null || _b === void 0 ? void 0 : _b.toLowerCase()) || "";
             return nome.includes(termo);
         });
         renderizarDados(filtrados);
     }
     inputBusca.addEventListener("input", filtrar);
     btnBusca.addEventListener("click", filtrar);
+});
+document.addEventListener("DOMContentLoaded", () => {
+    var _a, _b;
+    const closeModal = () => {
+        const modalOverlay = document.getElementById("modal-overlay");
+        modalOverlay.classList.add("hidden");
+    };
+    (_a = document.getElementById("modal-close")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", closeModal);
+    (_b = document.getElementById("modal-overlay")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (e) => {
+        if (e.target === e.currentTarget)
+            closeModal();
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById('navbar');
+    const searchIcon = document.getElementById('search-icon');
+    const timesIcon = document.getElementById('times-icon');
+    const searchContainer = document.getElementById('search-container');
+    searchIcon.addEventListener('click', () => {
+        searchContainer.classList.toggle('hidden');
+        timesIcon.classList.toggle('hidden');
+        navbar.classList.toggle('expanded');
+    });
+    timesIcon.addEventListener('click', () => {
+        searchContainer.classList.toggle('hidden');
+        timesIcon.classList.toggle('hidden');
+        navbar.classList.toggle('expanded');
+    });
 });
